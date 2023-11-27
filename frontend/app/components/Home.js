d12e8717639ceb9e8e01aff36739be4aabb32b47
mohitdhiman.zeroit <mohitdhiman.zeroit@gmail.com>
2023-11-27 18:11:12 +0530

    Changes

--- frontend/app/components/Home.js
+++ frontend/app/components/Home.js
@@ -1,104 +1,60 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React,{useState, useEffect} from 'react';
+import { Text, View, StyleSheet, ScrollView, } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+const Home =  ()  => {
+  const getItem = async () => {
+  const data = await AsyncStorage.getItem('key');
+      console.log(data,"yyyyyyyyyyy")
+  }
+  console.log(getItem(),"uuuuuuuu")
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const url = "https://jsonplaceholder.typicode.com/posts"
+  console.log(url,"hhhhhhhhhhh")
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
-const Home = async () => {
-  const message = { text: "Hello world!" };
-       const data = await AsyncStorage.getItem('key');
-    console.log(data,"yyyyyyyyyyyooooooooooooooooo")
-  return (
-    <>
-    <View style={styles.container}>
-      <Text>{message}</Text>
-    </View>
-    </>
-  );
+  }, []); 
+  <View style={styles.container}>
+    <ScrollView style={styles.scrollView}>
+  {loading ? (
+    <Text>Loading...</Text>
+  ) : (
+    data.map((post) => {
+      return (
+        <View>
+            
+          <Text style={styles.title}>{post.title}</Text>
+          <Text>{post.body}</Text>
+          
+        </View>
+       
+      );
+    })
+  )}
+   </ScrollView>    
+</View>;
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
+    padding: 8,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  scrollView: {
+    backgroundColor: 'blue',
+    marginHorizontal: 20,
   },
 });
 
 export default Home;
-// import React from 'react';
-// import { StyleSheet, Text, View, ImageBackground, Image, TouchableOpacity, Alert } from 'react-native';
-
-// export default class App extends React.Component {
-
-
-
-//   render() {
-//     return (
-//       <ImageBackground
-     
-//         style={styles.background}
-//       >
-//         <View>
-//           <Image
-           
-//             style={styles.logo}
-//             resizeMode="contain"
-//           >
-//           </Image>
-//           <Text style={styles.text}>Travel with people. Make new friends.</Text>
-//           <TouchableOpacity 
-//             onPress={this.signupPressed}
-//           >
-//             <Text style={styles.signup}>Sign Up</Text>
-//           </TouchableOpacity>
-//           <TouchableOpacity
-//             onPress={this.loginPressed}
-//           >
-//             <Text style={styles.login}>Log In</Text>
-//           </TouchableOpacity>
-//         </View>
-//       </ImageBackground>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//     background: {
-//       width: '100%',
-//       height: '100%'
-//     },
-//     logo:{
-//       width: 280,
-//       height: 280,
-//       marginLeft: '15%',
-//       marginTop: '10%'
-//     },
-//     text: {
-//       color: 'white',
-//       marginTop: '-25%',
-//       marginLeft: '20%'
-//     },
-//     signup: {
-//       backgroundColor: 'white',
-//       color: '#3A59FF',
-//       width: "75%",
-//       borderRadius: 25,
-//       textAlign: 'center',
-//       fontWeight: 'bold',
-//       marginLeft: '11%',
-//       padding: "2%",
-//       fontSize:  27,
-//       marginTop: '70%'
-//     },
-//     login: {
-//       backgroundColor: '#3A59FF',
-//       color: 'white',
-//       width: "75%",
-//       borderRadius: 25,
-//       textAlign: 'center',
-//       fontWeight: 'bold',
-//       marginLeft: '11%',
-//       padding: "2%",
-//       fontSize:  27,
-//       marginTop: '10%'
-//     }
-// });
